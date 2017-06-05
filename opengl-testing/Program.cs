9e91f6c5da4bf623242285d3ac1cb98c31f71567
Alex <dwt416@gmail.com>
2017-06-05 23:12:09 +0300

    Add project files.

---
+++ opengl-testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opengl_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

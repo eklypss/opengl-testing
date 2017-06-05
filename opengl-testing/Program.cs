7a51332a3df382be8329264dfd50fb58d707540e
Alex <dwt416@gmail.com>
2017-06-05 23:44:03 +0300

    First commit

--- opengl-testing/Program.cs
+++ opengl-testing/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES20;
+using OpenTK.Input;
 
 namespace opengl_testing
 {
-    class Program
+    internal class Program : GameWindow
     {
-        static void Main(string[] args)
+        public Program() : base(800, 600, GraphicsMode.Default, "OpenGL Testing", GameWindowFlags.FixedWindow)
         {
+            VSync = VSyncMode.Off;
+            CursorVisible = true;
+        }
+
+        private static void Main(string[] args)
+        {
+            using (Program pro = new Program())
+            {
+                pro.Run(30, 60);
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(Color.Firebrick);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            if (Keyboard[Key.Escape])
+            {
+                Exit();
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            SwapBuffers();
+        }
+
+        protected override void OnKeyDown(KeyboardKeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            Console.WriteLine($"{e.Key} was pressed.");
         }
     }
 }
